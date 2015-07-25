40b306161ba2a209d8996fb9c75c2215d9d7b8b6
wood-galaxy <wood.galaxy@gmail.com>
2015-07-25 22:44:00 +0200

    Publication sur le store

--- www/js/app.js
+++ www/js/app.js
@@ -27,55 +27,179 @@ $( document ).ready(function() {
     UI.init();
 
     var Connect = new XMLHttpRequest();
-    Connect.open("GET", "schedule.xml", false);
+    Connect.open("GET", "data/schedule.xml", false);
     Connect.setRequestHeader("Content-Type", "text/xml");
     Connect.send(null);
     TheDocument = Connect.responseXML;
-    console.log(TheDocument)
 
-    $('#currentdate').html(makeDateString());
+    $('#info-evenement').html(getConferenceInfos());
+    /*$('#currentdate').html(makeDateString());*/
+    $('#my-conf-list').html(refreshMyConfList());
 
-    $('#conf-list').html(getConfList(TheDocument));
-
-    os1 = UI.optionselector('os1');
-
-                os1.onClicked(function (e) {
-                    console.log("optionselector1 values: " + e.values);
-                })
-    os2 = UI.optionselector('os2');
-
-                os2.onClicked(function (f) {
-                    console.log("optionselector2 values: " + f.values);
-                })
+    $('#all-conf-list').html(getConfList(TheDocument));
 
 });
 
+function getConferenceInfos(){
+    var TheDocumentConference = TheDocument.getElementsByTagName("conference");
+    var title = TheDocumentConference[0].getElementsByTagName("title")[0].childNodes[0].nodeValue;
+    var venue = TheDocumentConference[0].getElementsByTagName("venue")[0].childNodes[0].nodeValue;
+    var city = TheDocumentConference[0].getElementsByTagName("city")[0].childNodes[0].nodeValue;
+    var start = TheDocumentConference[0].getElementsByTagName("start")[0].childNodes[0].nodeValue;
+    var end = TheDocumentConference[0].getElementsByTagName("end")[0].childNodes[0].nodeValue;
+    var res = '<h1>'+ title + '</h1>';
+    res += '<p>Lieu : '+ venue + '</p>';
+    res += '<p>À : ' + city + '</p>';
+    res += '<p>Du : ' + start + ' au : ' + end + '</p>';
+    return res
+}
+
 function getConfList( ){
-    var res = '<ul>';
+    var res ='';
     var TheDocumentRoom = TheDocument.getElementsByTagName("event");
     for (i=0;i<TheDocumentRoom.length;i++) {
         var conftitle = TheDocumentRoom[i].getElementsByTagName("title")[0].childNodes[0].nodeValue;
-        /*res += "<div><span id='confinfo"+ i +"' onclick='displayConfInfo(" + i + ")'>" + conftitle + '</span></div>';*/
-        res += "<li id='confinfo"+ i +"' onclick='displayConfInfo(" + i + ")'>" + conftitle + '</li>';
+        res += "<div><h2 id='conf-title"+ i +"' onclick='displayConfInfo(" + i + ")'>" + conftitle + "</h2>";
+        res += "<div id='conf-info" + i + "' style='display: none'></div><hr width='75%' align=left></div>";
       }
-    res = res + '</ul>';
     return res;
 };
 
-function displayConfInfo( i ){
+function getEventInfo( i ){
     var TheDocumentEvent = TheDocument.getElementsByTagName("event");
     var confTitle = TheDocumentEvent[i].getElementsByTagName("title")[0].childNodes[0].nodeValue;
-    var confAbstract = TheDocumentEvent[i].getElementsByTagName("abstract")[0].childNodes[0].nodeValue;
-    var confDescription = TheDocumentEvent[i].getElementsByTagName("description")[0].childNodes[0].nodeValue;
-    var res = '<p>'+ confTitle + '</p>';
-    res += '<p>'+ confAbstract + '</p>';
-    res += '<p>'+ confDescription + '</p>';
-    $("#confinfo"+i).html(res);
-    /*$('html,body').animate({scrollTop: 0}, 'slow');*/
+    var eventSpeakers = TheDocumentEvent[i].getElementsByTagName("persons");
+    if(typeof(TheDocumentEvent[i].getElementsByTagName("start")[0].childNodes[0]) !== "undefined") {
+      var confStart = TheDocumentEvent[i].getElementsByTagName("start")[0].childNodes[0].nodeValue;
+    }
+    if(typeof(TheDocumentEvent[i].getElementsByTagName("duration")[0].childNodes[0]) !== "undefined") {
+      var confDuration = TheDocumentEvent[i].getElementsByTagName("duration")[0].childNodes[0].nodeValue;
+    }
+    if(typeof(TheDocumentEvent[i].getElementsByTagName("room")[0].childNodes[0]) !== "undefined") {
+      var confRoom = TheDocumentEvent[i].getElementsByTagName("room")[0].childNodes[0].nodeValue;
+    }
+    if(typeof(TheDocumentEvent[i].getElementsByTagName("track")[0].childNodes[0]) !== "undefined") {
+      var confTrack = TheDocumentEvent[i].getElementsByTagName("track")[0].childNodes[0].nodeValue;
+    }
+    if(typeof(TheDocumentEvent[i].getElementsByTagName("abstract")[0].childNodes[0]) !== "undefined") {
+      var confAbstract = TheDocumentEvent[i].getElementsByTagName("abstract")[0].childNodes[0].nodeValue;
+    }
+    if(typeof(TheDocumentEvent[i].getElementsByTagName("description")[0].childNodes[0]) !== "undefined") {
+      var confDescription = TheDocumentEvent[i].getElementsByTagName("description")[0].childNodes[0].nodeValue;
+    }
+    var res = '<p>Par :';
+    for (j=0;j<eventSpeakers[0].getElementsByTagName("person").length;j++) {
+        var speaker = eventSpeakers[0].getElementsByTagName("person")[j].childNodes[0].nodeValue;
+        if(j>0){
+          res += ' et ' + speaker;
+        }
+        else {
+          res += ' ' + speaker;
+        }
+    }
+    res += '</p>'
+    if(confStart && confDuration) {
+        res += '<p>Début : '+ confStart + ' - Durée : ' + confDuration + '</p>';
+    }
+    if(confTrack && confRoom) {
+        res += '<p>Track : '+ confTrack + ' - Salle : ' + confRoom + '</p>';
+    }
+    if(confAbstract) {
+      res += '<p>'+ confAbstract + '</p>';
+    }
+    if(confDescription) {
+      res += '<p>'+ confDescription + '</p>';
+    }
+    return res
+}
+
+function getSpeakersList(){
+    var TheDocumentSpeakers = TheDocument.getElementsByTagName("person");
+    console.log(TheDocumentSpeakers);
+};
+
+function displayConfInfo( i ){
+    var res = getEventInfo( i );
+    res += "<button data-role='button' class='positive' onclick='addConf(" + i + ")'>Ajouter à mon programme</button>"
+    $("#conf-info"+i).html(res);
+    var ele = document.getElementById("conf-info"+i);
+    var text = document.getElementById("conf-title"+i);
+    if(ele.style.display === "block") {
+      ele.style.display = "none";
+    }
+    else {
+      ele.style.display = "block";
+    }
+};
+
+function displayMyConfInfo( i ){
+    var res = getEventInfo( i );
+    res += "<button data-role='button' class='negative positive' onclick='delConf(" + i + ")'>Suprimmer du programme</button>"
+    $("#myconf-info"+i).html(res);
+    var ele = document.getElementById("myconf-info"+i);
+    var text = document.getElementById("myconf-title"+i);
+    if(ele.style.display === "block") {
+      ele.style.display = "none";
+    }
+    else {
+      ele.style.display = "block";
+    }
 };
 
 function makeDateString() {
       var d = new Date();
       var dString = d.getFullYear() + "-" + d.getMonth() + "-" + d.getDate();
       return dString;
+};
+
+function refreshMyConfList(){
+    var res = ''
+    if(typeof(Storage) !== "undefined") {
+        if (localStorage.myconf) {
+            var myconfsplit = localStorage.myconf.split(" ");
+            for(var i = myconfsplit.length; i--;){
+                if (myconfsplit[i] === "") {
+                    myconfsplit.splice(i, 1)
+                }
+            };
+            var TheDocumentRoom = TheDocument.getElementsByTagName("event");
+            for (i=0; i < myconfsplit.length; i++){
+                var confid = myconfsplit[i]
+                var conftitle = TheDocumentRoom[confid].getElementsByTagName("title")[0].childNodes[0].nodeValue;
+                res += "<div><h2 id='myconf-title"+ confid +"' onclick='displayMyConfInfo(" + confid + ")'>" + conftitle + "</h2>";
+                res += "<div id='myconf-info" + confid + "' style='display: none'></div><hr width='75%' align=left></div>";
+            }
+            $("#my-conf-list").html(res);
+        } else {
+            $("#my-conf-list").html("Pas de conférences.");
+        }
+    }
+}
+
+function addConf( i ){
+    if(typeof(Storage) !== "undefined") {
+        if (localStorage.myconf) {
+            localStorage.myconf = localStorage.myconf + ' ' + i.toString() + ' ';
+        } else {
+            localStorage.myconf =  ' ' + i.toString() + ' ';
+        }
+    }
+    refreshMyConfList()
+};
+
+function delConf( i ){
+    if(typeof(Storage) !== "undefined") {
+        if (localStorage.myconf) {
+            var mynewconf = localStorage.myconf.replace(' ' + i.toString() + ' ','');
+            localStorage.myconf = mynewconf;
+        }
+    }
+    refreshMyConfList()
+};
+
+function delAllConf(  ){
+    if(typeof(Storage) !== "undefined") {
+        localStorage.clear();
+    }
+    refreshMyConfList()
 };
